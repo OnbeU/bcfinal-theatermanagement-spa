ae35e317523e182775841d27b2f848cafa87008a
J.R. Jenks <jrjenks@swiftprepaid.com>
2021-08-04 09:52:08 -0500

    Dashboard is now the default page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
+import { MovieListComponent } from './areas/movies/pages/movie-list/movie-list.component';
+import { DashboardComponent } from './areas/dashboard/pages/dashboard/dashboard.component';
 import { PageNotFoundComponent } from './areas/404/pages/page-not-found/page-not-found.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  {path: '' , component: DashboardComponent  },
+  {path: 'movies' , component: MovieListComponent },
   {path: '404', component: PageNotFoundComponent},
   {path: '**', redirectTo: '/404'}
 
