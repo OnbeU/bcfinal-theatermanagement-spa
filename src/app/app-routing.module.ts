5dc2490dac13ab7807d410d955691c3fb38ca75f
J.R. Jenks <jrjenks@swiftprepaid.com>
2021-08-04 10:22:39 -0500

    Movie list page appears

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,13 @@
-import { MovieListComponent } from './areas/movies/pages/movie-list/movie-list.component';
-import { DashboardComponent } from './areas/dashboard/pages/dashboard/dashboard.component';
-import { PageNotFoundComponent } from './areas/404/pages/page-not-found/page-not-found.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './areas/dashboard/pages/dashboard/dashboard.component';
+import { PageNotFoundComponent } from './areas/404/pages/page-not-found/page-not-found.component';
+import { MovieListComponent } from './areas/movies/pages/movie-list/movie-list.component';
 
 const routes: Routes = [
   {path: '' , component: DashboardComponent  },
-  {path: 'movies' , component: MovieListComponent },
   {path: '404', component: PageNotFoundComponent},
+  {path: 'movies', component: MovieListComponent},
   {path: '**', redirectTo: '/404'}
 
 ];
