e752b1eeeb69c9dd55b4ba5c2af90cd8dd832d1e
J.R. Jenks <jrjenks@swiftprepaid.com>
2021-08-04 07:22:20 -0500

    ng g c dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MovieListComponent } from './areas/movies/pages/movie-list/movie-list.component';
+import { DashboardComponent } from './areas/dashboard/pages/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MovieListComponent
+    MovieListComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
