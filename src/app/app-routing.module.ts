c59ff1971c2ae0880ced7782eea940f17091bbef
J.R. Jenks <jrjenks@swiftprepaid.com>
2021-08-04 08:45:36 -0500

    Dashboard is now the default page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
+import { DashboardComponent } from './areas/dashboard/pages/dashboard/dashboard.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  {path: '' , component: DashboardComponent  }
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
