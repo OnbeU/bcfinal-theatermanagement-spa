7760e05085241ebbcfcdf98c6745c09fa624ede1
J.R. Jenks <jrjenks@swiftprepaid.com>
2021-08-04 09:44:52 -0500

    Redirection to /404

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
-import { DashboardComponent } from './areas/dashboard/pages/dashboard/dashboard.component';
+import { PageNotFoundComponent } from './areas/404/pages/page-not-found/page-not-found.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {path: '' , component: DashboardComponent  }
+  {path: '404', component: PageNotFoundComponent},
+  {path: '**', redirectTo: '/404'}
+
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
