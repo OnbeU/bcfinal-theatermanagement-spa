4286eca4bced6df683262c9e11180296b71eb65b
J.R. Jenks <jrjenks@swiftprepaid.com>
2021-08-08 21:18:47 -0500

    Copy karma.pact.conf.js karma.conf.js

---
+++ karma.pact.conf.js
@@ -0,0 +1,65 @@
+// Karma configuration file, see link for more information
+// https://karma-runner.github.io/1.0/config/configuration-file.html
+var path = require("path")
+
+module.exports = function (config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular', 'pact'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage'),
+      require('@angular-devkit/build-angular/plugins/karma'),
+      require('@pact-foundation/karma-pact'),
+    ],
+
+    // Pact Providers
+    pact: [
+      {
+        port: 1234,
+        consumer: "bcfinal-theatermanagement-spa",
+        provider: "bcfinal-theatermanagement-bff",
+        logLevel: "DEBUG",
+        // log: path.resolve(process.cwd(), "logs", "pact.log"),
+        dir: path.resolve(process.cwd(), "pacts"),
+      },
+    ],
+
+    proxies: {'/api':'http://localhost:1234/hello-service/api'},
+
+    // proxies: {
+    //   '/api/': 'http://127.0.0.1:1234/api/'
+    // },
+
+    client: {
+      jasmine: {
+        // you can add configuration options for Jasmine here
+        // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
+        // for example, you can disable the random execution with `random: false`
+        // or set a specific seed with `seed: 4321`
+      },
+      clearContext: false // leave Jasmine Spec Runner output visible in browser
+    },
+    jasmineHtmlReporter: {
+      suppressAll: true // removes the duplicated traces
+    },
+    coverageReporter: {
+      dir: require('path').join(__dirname, './coverage/project'),
+      subdir: '.',
+      reporters: [
+        { type: 'html' },
+        { type: 'text-summary' }
+      ]
+    },
+    reporters: ['progress', 'kjhtml'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ['Chrome'],
+    singleRun: false,
+    restartOnFileChange: true
+  });
+};
