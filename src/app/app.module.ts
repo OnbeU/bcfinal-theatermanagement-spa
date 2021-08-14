e399f252e9780579b154a8caadfb41461291d7c6
J.R. Jenks <jrjenks@swiftprepaid.com>
2021-08-14 06:07:06 -0500

    A little code cleanup.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,25 +4,23 @@ import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { environment } from 'src/environments/environment';
 
-// TODO: Alphabetize these
+import { AboutComponent } from './areas/about/pages/about/about.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BackendService } from './core/singleton-services/backend/backend.service';
-import { MovieListComponent } from './areas/movies/pages/movie-list/movie-list.component';
 import { DashboardComponent } from './areas/dashboard/pages/dashboard/dashboard.component';
 import { HeaderComponent } from './areas/app/header/header.component';
+import { MovieListComponent } from './areas/movies/pages/movie-list/movie-list.component';
 import { PageNotFoundComponent } from './areas/404/pages/page-not-found/page-not-found.component';
-import { AboutComponent } from './areas/about/pages/about/about.component';
 
 @NgModule({
   declarations: [
-    // TODO: Alphabetize these
+    AboutComponent,
     AppComponent,
-    MovieListComponent,
     DashboardComponent,
     HeaderComponent,
+    MovieListComponent,
     PageNotFoundComponent,
-    AboutComponent
   ],
   imports: [
     BrowserModule,
