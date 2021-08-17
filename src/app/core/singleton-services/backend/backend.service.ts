a2624090a5391c9e016f0dce43dea31a970d4831
J.R. Jenks <jrjenks@swiftprepaid.com>
2021-08-16 22:34:08 -0500

    Can now generate a Pact with npm run singletestrun

--- src/app/core/singleton-services/backend/backend.service.ts
+++ src/app/core/singleton-services/backend/backend.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
 
 import { Movie } from './movie';
 import { ConfigService } from './../config/config.service';
@@ -20,30 +20,6 @@ export class BackendService {
 
   getMovies(): Observable<Movie[]> {
     const moviesUrl = `${this.apiBaseUrl}/movies`;
-    console.log(`**** moviesUrl ${moviesUrl}`);
-
-    // this.httpClient.get<string>(moviesUrl)
-    // .subscribe(content => {
-    //   console.log(`**** content ${content}`);
-    // });
-
-    // return of([]);
     return this.httpClient.get<Movie[]>(moviesUrl);
   }
-
-  getMoviesAsJson(): Observable<string> {
-    const moviesUrl = `${this.apiBaseUrl}/movies`;
-    console.log(`**** getMoviesAsJson moviesUrl ${moviesUrl}`);
-    var action = this.httpClient.get<string>(moviesUrl);
-    action.subscribe(
-      result => {
-        console.log('QQQQQQQQQQQ result is ' + result.toString());
-      },
-      err => {
-        console.log('QQQQQQQQQQQ err is ' + err.toString());
-      }
-    )
-    return action;
-  }
-
 }
